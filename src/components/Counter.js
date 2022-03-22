65282e05fa9f93741015edffc4c9e8e461715d07
Andres Bernal <publiminal@gmail.com>
2022-03-21 23:07:50 -0400

    added Counter component functionality. test did not pass successfully yet

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,10 +46,11 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React , {useState} from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
+const [count, setCount] = useState(0)
 
   const increment = () => {
     /* STEP 4 */
@@ -61,22 +62,30 @@ export default function Counter() {
     /* STEP 6 */
   };
 
+  function isEven(val) {
+    return val%2 === 0
+  }
+
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: isEven(count) ? 'royalblue' :  'crimson' , // 'royalblue', /* STEP 2 */
+    transition: 'color .25s ease'
   };
+  // console.log('color' , style.color)
+
+
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is { isEven()? 'even' : 'odd'    /* STEP 3 */}
       </div>
       <div>
-        <button id='increment' onClick={increment}>Increment</button>
-        <button id='decrement' onClick={decrement}>Decrement</button>
-        <button id='resetCount' onClick={reset}>Reset</button>
+        <button id='increment' onClick={() => setCount(count + 1)}>Increment</button>
+        <button id='decrement' onClick={() => setCount(count - 1)}>Decrement</button>
+        <button id='resetCount' onClick={() => setCount(0) }>Reset</button>
       </div>
     </div>
   );
